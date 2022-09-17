7e286c98e8c1bc911f3d1bdff25b612d184728cd
Matyrobbrt <matyrobbrt@gmail.com>
2022-09-17 22:27:00 +0300

    Leave + join logging

---
+++ src/main/groovy/com/matyrobbrt/kaupenbot/extensions/JoinsExtension.groovy
@@ -0,0 +1,59 @@
+package com.matyrobbrt.kaupenbot.extensions
+
+import com.matyrobbrt.kaupenbot.KaupenBot
+import groovy.transform.CompileStatic
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.entities.IMentionable
+import net.dv8tion.jda.api.entities.MessageChannel
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent
+import net.dv8tion.jda.api.utils.TimeFormat
+
+import java.awt.Color
+
+@CompileStatic
+@RegisterExtension
+final class JoinsExtension implements BotExtension {
+    @Override
+    void subscribeEvents(JDA jda) {
+        jda.subscribe(GuildMemberJoinEvent) {
+            final role = KaupenBot.jda.getRoleById(KaupenBot.config.joinRole)
+            if (role !== null) guild.addRoleToMember(member, role).reason('Join Role').queue()
+
+            final log = getLogChannel(jda)
+            if (log !== null) {
+                final embed = new EmbedBuilder()
+                        .sentNow()
+                        .setColor(Color.GREEN)
+                        .setTitle("User Joined")
+                        .setFooter("User ID: " + member.getId())
+                        .addField("User:", "$user.asTag ($user.asMention)", true)
+                        .setThumbnail(member.getEffectiveAvatarUrl())
+                        .addField("Joined Discord:", TimeFormat.DATE_TIME_SHORT.format(user.getTimeCreated()), true)
+                        .build()
+                log.sendMessageEmbeds(embed).queue()
+            }
+        }
+        jda.subscribe(GuildMemberRemoveEvent) {
+            final log = getLogChannel(jda)
+            if (log !== null) {
+                final embed = new EmbedBuilder()
+                        .sentNow()
+                        .setColor(Color.RED)
+                        .setTitle('User Left')
+                        .setFooter('User ID: ' + member.getId(), member.getEffectiveAvatarUrl())
+                        .addField('User:', user.getAsTag(), true)
+                        .addField('Join Time:', TimeFormat.DATE_TIME_SHORT.format(member.getUser().getTimeCreated()), true)
+                        .addField('Roles', IMentionable.orNone(member.getRoles()), false)
+                        .setThumbnail(user.getAvatarUrl())
+                        .build()
+                log.sendMessageEmbeds(embed).queue()
+            }
+        }
+    }
+
+    static MessageChannel getLogChannel(JDA jda) {
+        return jda.getChannelById(MessageChannel, KaupenBot.config.loggingChannels.leaveJoinLogs)
+    }
+}
